52302a8322d0c1293c127e88863189e21b44502a
SomeBottle <somebottle@outlook.com>
2021-09-11 23:09:56 +0800

    Update search.js

--- template/search.js
+++ template/search.js
@@ -4,7 +4,7 @@ B.githubrepo = "";/*博客所在github仓库，比如我的在github.com/SomeBot
 B.searchinj = (postindexes, searchword, callback) => {
     if (B.githubrepo !== "") {
         let queryString = encodeURIComponent(searchword) + "+in:file+language:html+repo:" + B.githubrepo;
-        $.aj("https://api.github.com/search/code?q=" + queryString, {}, {
+        $.ft("https://api.github.com/search/code?q=" + queryString, {}, {
             success: (j) => {
                 let rs = JSON.parse(j), items = rs['items'], resultarr = [];
                 for (var o in items) resultarr.push(parseInt(items[o]["name"].replace("post-", "").replace(".html", "")));
@@ -12,6 +12,6 @@ B.searchinj = (postindexes, searchword, callback) => {
             }, failed: (j) => {
                 console.log('API Search failed');
             }
-        }, 'get', '', true);
+        }, 'get', '');
     }
 }
